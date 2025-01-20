59e7aadcd2c573419bfc6d18b97728058d69a5b1
Daniel Pereira Lima <limadanielpereira@gmail.com>
2025-01-20 16:18:08 -0300

    adiciona Pesquisa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import Header from "./componentes/Header";
 import styled from "styled-components";
-
+import Pesquisa from "./componentes/Pesquisa";
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002f52 35%, #326589);
-
 `;
 
 export default function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
-
-
