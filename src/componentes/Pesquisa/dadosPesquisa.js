6f4f1e3246e574bb111199ddb320525a98d27105
Daniel Pereira Lima <limadanielpereira@gmail.com>
2025-01-20 16:41:24 -0300

    adiciona Pesquisa

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from "../../imagens/livro.png";
+
+export const livros = [
+  { nome: "Liderança em Design", id: "1", src: livro },
+  { nome: "Algoritmos", id: "2", src: livro },
+  { nome: "Código Limpo", id: "3", src: livro },
+  { nome: "Learning to Learn", id: "4", src: livro },
+  { nome: "JavaScript Acertivo", id: "5", src: livro },
+];
+
