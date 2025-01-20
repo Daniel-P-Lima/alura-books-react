29016d7c00afd3c3d107ca64becc46de4285ff9e
Daniel Pereira Lima <limadanielpereira@gmail.com>
2025-01-20 15:01:34 -0300

    remove .css e add containers

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,16 +1,23 @@
 import "./estilo.css";
 import logo from "../../imagens/logo.svg";
+import styled from "styled-components";
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`;
+
+const ImagemLogo = styled.img`
+  margin-right: 10px;
+`;
 
 export default function Logo() {
   return (
-    <div className="logo">
-      <img src={logo} 
-      alt="logo" 
-      className="logo-img"></img>
+    <LogoContainer>
+      <ImagemLogo src={logo} alt="logo"></ImagemLogo>
       <p>
         <strong>Alura</strong>Books
       </p>
-    </div>
+    </LogoContainer>
   );
 }
-
