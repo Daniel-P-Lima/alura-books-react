420f1fe1b2294e9c5f7ca579b2b388b6002d5171
Daniel Pereira Lima <limadanielpereira@gmail.com>
2025-01-20 14:59:06 -0300

    Adiciona IconesContaier, Icone e remove .css

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,18 +1,26 @@
-import "./estilo.css";
 import perfil from "../../imagens/perfil.svg";
 import sacola from "../../imagens/sacola.svg";
+import styled from "styled-components";
+
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+`;
+
+const IconesContainer = styled.ul`
+  display: flex;
+  align-items: center;
+`;
 
 const icones = [perfil, sacola];
 export default function IconesHeader() {
   return (
-    <ul className="icones">
+    <IconesContainer>
       {icones.map((icone) => (
-        <li className="icone">
+        <Icone>
           <img src={icone} alt="imagem"></img>
-        </li>
+        </Icone>
       ))}
-    </ul>
+    </IconesContainer>
   );
 }
-
-
