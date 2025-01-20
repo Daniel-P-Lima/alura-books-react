b6c3c6f0c158ec398a72130b8900cdb867c86033
Daniel Pereira Lima <limadanielpereira@gmail.com>
2025-01-20 14:32:52 -0300

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Logo from "./componentes/Logo";
+import Header from "./componentes/Header"
+import styled from 'styled-components'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo></Logo>
-      </header>
+    <div className="app">
+      <Header/>
     </div>
   );
 }
